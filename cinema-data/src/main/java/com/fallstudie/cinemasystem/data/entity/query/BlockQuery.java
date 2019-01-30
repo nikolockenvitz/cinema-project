fb422e133ab32e399cafc56374a54e2771bfa4a2
Sascha Görnert <sascha@wirtschaftsinformatik.se>
2019-01-30 15:30:39 +0100

    - next try for Block

--- cinema-data/src/main/java/com/fallstudie/cinemasystem/data/entity/query/BlockQuery.java
+++ cinema-data/src/main/java/com/fallstudie/cinemasystem/data/entity/query/BlockQuery.java
@@ -3,7 +3,7 @@ package com.fallstudie.cinemasystem.data.entity.query;
 public interface BlockQuery
 {
     public static final String FIND_BLOCKEDSEATS_BY_SHOW_ID = "SELECT b FROM Block b" + " WHERE b.show.id =:" + QueryParam.SHOW_ID
-            + " AND b.timeofreservation > NOW() - INTERVAL '5 min'";
+            + " AND b.timeofreservation > SQL(\"(? - interval '5' minute)\", CURRENT_TIMESTAMP)";
 
     public static final String FIND_BLOCKEDSEATS_BY_SHOW_ID_QNAME = BlockQuery.FIND_BLOCKEDSEATS_BY_SHOW_ID;
 
